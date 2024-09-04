474c6973b34ea103b235492046100eba017a9daa
fisheeesh <swanphyo444@gmail.com>
2024-09-04 14:20:55 +0700

    json and callback hell

--- warmup/callback.js
+++ warmup/callback.js
@@ -1,45 +1,49 @@
+let getTodos = (resource, callback) => {
+  let request = new XMLHttpRequest();
+
+  request.addEventListener("readystatechange", () => {
+    if (request.readyState === 4 && request.status === 200) {
+      let datas = JSON.parse(request.responseText);
+      callback(datas, undefined);
+    } else if (request.status === 404) {
+      callback(undefined, "404 not found");
+    }
+  });
+
+  request.open("GET", resource);
+  request.send();
+};
+
+//callback hell - callback inside callback, it is hard to hide and reduce performance
+//avoid using callback hell
+getTodos("../warmup/json/syp.json",(data, err) => {
+  if (data) {
+    console.log(data)
+    getTodos('../warmup/json/test.json', (data, err)=>{
+        if(data) console.log(data)
+        else console.log(err)
+    })
+  } else console.log(err);
+});
+
 // let getTodos = (callback) => {
-//   let request = new XMLHttpRequest();
-
-//   request.addEventListener("readystatechange", () => {
-//     if (request.readyState === 4 && request.status === 200) {
-//       callback(request.response, undefined);
-//     } else if (request.status === 404) {
-//       callback(undefined, "404 not found");
-//     }
-//   });
-
-//   request.open("GET", "https://jsonplaceholder.typicode.com/todos");
-//   request.send();
+//   fetch("https://jsonplaceholder.typicode.com/todos")
+//     .then((response) => response.json())
+//     .then((data) => callback(data, undefined))
+//     .catch((err) => callback(undefined, err));
 // };
 
-// getTodos((data, err) =>{
-//     if(data){
+// getTodos((data, err)=>{
+//     if(data) {
 //         let userInput = prompt('Search by title: ')
 //         let filteredData = data.filter(user => user.title.includes(userInput))
-//         console.log(filteredData)
+//         filteredData.map(user => console.log(user.userId, user.title))
 //     }
 //     else console.log(err)
 // })
 
-let getTodos = (callback) => {
-  fetch("https://jsonplaceholder.typicode.com/todos")
-    .then((response) => response.json())
-    .then((data) => callback(data, undefined))
-    .catch((err) => callback(undefined, err));
-};
-
-getTodos((data, err)=>{
-    if(data) {
-        let userInput = prompt('Search by title: ')
-        let filteredData = data.filter(user => user.title.includes(userInput))
-        filteredData.map(user => console.log(user.userId, user.title))
-    }
-    else console.log(err)
-})
-
 // async function getTodos(callback){
-//     let response = await fetch('https://jsonplaceholder.typicode.com/todosk')
+//     let response = await fetch('https://jsonplaceholder.typicode.com/todos')
 //     let data = await response.json()
 //     callback(data, undefined)
 // }
