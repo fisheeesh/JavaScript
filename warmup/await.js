5cf81daf011943b664916428ef7d8851168b0af5
fisheeesh <swanphyo444@gmail.com>
2024-09-04 15:18:43 +0700

    warm-up finished

--- warmup/await.js
+++ warmup/await.js
@@ -1,6 +1,13 @@
 let fetchData = async() => {
+    /**
+     * fetch will return resolve whether it is true or false
+     * so that means we have to handle reject case which is error handling manually
+     */
     let response = await fetch('../warmup/json/syp.json')
-    let data = await response.json()
-    console.log(data)
+    if(response.status === 404) throw new Error('Ur link is not available.!')
+    let datas = await response.json()
+    return datas
 }
 fetchData()
+.then(data => console.log(data))
+.catch(err => console.log(err.message))
