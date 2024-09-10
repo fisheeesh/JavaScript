7dbb28bf24a8ed1837f2786c14113201091905d8
fisheeesh <swanphyo444@gmail.com>
2024-09-11 00:04:21 +0700

    find method

---
+++ app.js
@@ -0,0 +1,9 @@
+let projects = [
+    {id : 1, name : 'Project 1', details : 'Project 1 Details'},
+    {id : 2, name : 'Project 2', details : 'Project 2 Details'},
+    {id : 3, name : 'Project 3', details : 'Project 3 Details'},
+]
+
+let project = projects.find(project => project.id === 1)
+
+console.log(project)
